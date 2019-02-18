edf4e01f1424c995f4ba93fadb9b7c7ffb65f7c9
reromero <reromero@compusoluciones.com>
2019-02-18 12:28:04 -0600

    Naive Bayes exercise finished but it still needs a little more testing

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,4 +1,4 @@
-def exercise_frequency_table():
+def naive_bayes(grade, hours):
     grades_exercise_table = {
         "A_No":1/3,
         "A_Yes":1/3,
@@ -14,8 +14,6 @@ def exercise_frequency_table():
         "Total":1
     }
 
-    print(grades_exercise_table)
-
     hours_exercise_table ={
         "1.0_No":2/3,
         "1.0_Yes":0,
@@ -26,18 +24,26 @@ def exercise_frequency_table():
         "3.0_No":0,
         "3.0_Yes":1/3,
         "3.0_Total":1/6,
+        "No_Total":3/6,
+        "Yes_Total":3/6,
         "Total":1
     }
 
-    print(hours_exercise_table)
+    dividend_no = (grades_exercise_table["No_Total"])*(grades_exercise_table[grade + "_No"]) * (hours_exercise_table[str(hours) + "_No"])
+    dividend_yes = (grades_exercise_table["Yes_Total"])*(grades_exercise_table[grade + "_Yes"]) * (hours_exercise_table[str(hours) + "_Yes"])
+    divisor = (grades_exercise_table[grade + "_Total"])*(hours_exercise_table[str(hours) + "_Total"])
 
-def naive_bayes(grade, hours):
-    pass
-
+    yes_prob = dividend_yes/divisor
+    no_prob = dividend_no/divisor
+    
+    if yes_prob > no_prob:
+        print("You will pass!!")
+    else:
+        print("You will not pass /:")
 
 def main():
     grade = input("Enter the grade:\n")
-    hours = input("Enter the ammount of hours:\n")
-    exercise_frequency_table()
+    hours = float(input("Enter the ammount of hours:\n"))
+    naive_bayes(grade, hours)
 
 main()
